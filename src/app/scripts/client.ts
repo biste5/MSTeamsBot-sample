b7eb5dbb27998d4258df39fd7edc584e5c03f093
Massimo Prota <massimo.prota@rapidcircle.com>
2017-05-09 15:54:35 -0700

    First Commit

---
+++ src/app/scripts/client.ts
@@ -0,0 +1,11 @@
+// Default entry point for client scripts
+// Automatically generated
+// Please avoid from modifying to much...
+
+
+// Added by generator-teams
+export * from './teamsBuildTabConfig';
+export * from './teamsBuildTabTab';
+
+// Added by generator-teams
+export * from './teamsBldTabTab';
---
+++ src/app/scripts/teamsBuildTabConfig.ts
@@ -0,0 +1,40 @@
+import {TeamsTheme} from './theme';
+
+/**
+ * Implementation of Teams Build Tab configuration page
+ */
+export class teamsBuildTabConfigure {
+    constructor() {
+        microsoftTeams.initialize();
+
+        microsoftTeams.getContext((context:microsoftTeams.Context) => {
+            TeamsTheme.fix(context);
+            let val = <HTMLInputElement>document.getElementById("data");
+            if (context.entityId) {
+                val.value = context.entityId;
+            }
+            this.setValidityState(true);
+        });
+		
+        microsoftTeams.settings.registerOnSaveHandler((saveEvent: microsoftTeams.settings.SaveEvent) => {
+
+            let val = <HTMLInputElement>document.getElementById("data");
+			// Calculate host dynamically to enable local debugging
+			let host = "https://" + window.location.host;
+			let defaultTabName: string = `MSTeamsBot sample`;
+			// Upper case first letter of tab name
+			defaultTabName = defaultTabName.charAt(0).toUpperCase() + defaultTabName.slice(1);
+            microsoftTeams.settings.setSettings({
+                contentUrl: host + "/teamsBuildTabTab.html?data=",
+                suggestedDisplayName: defaultTabName,
+                removeUrl: host + "/teamsBuildTabRemove.html",
+				entityId: val.value
+            });
+
+            saveEvent.notifySuccess();
+        });
+    }
+    public setValidityState(val: boolean) {
+        microsoftTeams.settings.setValidityState(val);
+    }
+}
